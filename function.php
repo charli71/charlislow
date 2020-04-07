3a43841b719a849b7b1496be6b3ab389e5dc0d69
charli71 <61778068+charli71@users.noreply.github.com>
2020-04-08 00:32:32 +0700

    Update function.php

--- function.php
+++ function.php
@@ -8,8 +8,8 @@ $header[] = "Content-Type: application/json; charset=UTF-8";
 $header[] = "X-AppVersion: 3.46.1";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
-$header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -6.220".mt_rand(100,999).",106.77".mt_rand(1000,9999);
+$header[] = "X-User-Locale: id_ID"; -6.917464,107.619122";
+$header[] = "X-Location:  ;
 if ($pin):
 $header[] = "pin: $pin";
     endif;
